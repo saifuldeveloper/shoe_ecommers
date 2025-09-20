086729415e737f50f457b317743352806a8a4eeb
alamin <alamincsetpi@gmail.com>
2025-09-20 21:17:00 +0600

    feat: add the prev next btn

--- resources/views/front/pages/product_detail.blade.php
+++ resources/views/front/pages/product_detail.blade.php
@@ -480,22 +480,23 @@
         </div>
     </div>
  
-    {{-- //recently view products --}}
-     <div class="ps-section">
+
+     {{-- might like image  --}}
+    <div class="ps-section ps-section--top-sales ps-owl-root pt-40 pb-80">
         <div class="ps-container">
-            <div class="ps-section__header">
+            <div class="ps-section__header mb-50">
                 <div class="row">
                     <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12">
-                        <h3 class="ps-section__title" >
-                            Recently Viewed Products
+                        <h3 class="ps-section__title" data-mask="">
+                            - RECENTLY VIEW PRODUCTS
                         </h3>
                     </div>
-                    {{-- <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
+                    <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12">
                         <div class="ps-owl-actions">
                             <a class="ps-prev" href="#"><i class="ps-icon-arrow-right"></i>Prev</a><a
                                 class="ps-next" href="#">Next<i class="ps-icon-arrow-left"></i></a>
                         </div>
-                    </div> --}}
+                    </div>
                 </div>
             </div>
             <div class="ps-section__content">
@@ -506,7 +507,9 @@
                     <div class="ps-shoes--carousel">
                         <div class="ps-shoe">
                             <div class="ps-shoe__thumbnail">
-                               
+                                <div class="ps-badge">
+                                    <span>New</span>
+                                </div>
                                 <a class="ps-shoe__favorite" href="#"><i class="ps-icon-heart"></i></a><img
                                     src="{{ asset('assets/frontend/images/shoe/a1.avif') }}" alt="" /><a
                                     class="ps-shoe__overlay" href="product-detail.html"></a>
@@ -514,11 +517,9 @@
                             <div class="ps-shoe__content">
                                 <div class="ps-shoe__variants">
                                     <div class="ps-shoe__variant normal">
-                                        <img 
-                                        src="{{ asset('assets/frontend/images/shoe/2.jpg') }}" alt="" /><img
+                                        <img src="{{ asset('assets/frontend/images/shoe/2.jpg') }}" alt="" /><img
                                             src="images/shoe/3.jpg" alt="" /><img src=""
-                                            alt="" />
-                                            <img src="{{ asset('assets/frontend/images/shoe/4.jpg') }}"
+                                            alt="" /><img src="{{ asset('assets/frontend/images/shoe/4.jpg') }}"
                                             alt="" />
                                     </div>
                                     <select class="ps-rating ps-shoe__rating">
@@ -544,7 +545,9 @@
                      <div class="ps-shoes--carousel">
                         <div class="ps-shoe">
                             <div class="ps-shoe__thumbnail">
-                              
+                                <div class="ps-badge">
+                                    <span>New</span>
+                                </div>
                                 <a class="ps-shoe__favorite" href="#"><i class="ps-icon-heart"></i></a><img
                                     src="{{ asset('assets/frontend/images/shoe/a1.avif') }}" alt="" /><a
                                     class="ps-shoe__overlay" href="product-detail.html"></a>
@@ -552,11 +555,9 @@
                             <div class="ps-shoe__content">
                                 <div class="ps-shoe__variants">
                                     <div class="ps-shoe__variant normal">
-                                        <img 
-                                        src="{{ asset('assets/frontend/images/shoe/2.jpg') }}" alt="" /><img
+                                        <img src="{{ asset('assets/frontend/images/shoe/2.jpg') }}" alt="" /><img
                                             src="images/shoe/3.jpg" alt="" /><img src=""
-                                            alt="" />
-                                            <img src="{{ asset('assets/frontend/images/shoe/4.jpg') }}"
+                                            alt="" /><img src="{{ asset('assets/frontend/images/shoe/4.jpg') }}"
                                             alt="" />
                                     </div>
                                     <select class="ps-rating ps-shoe__rating">
@@ -582,7 +583,9 @@
                      <div class="ps-shoes--carousel">
                         <div class="ps-shoe">
                             <div class="ps-shoe__thumbnail">
-                            
+                                <div class="ps-badge">
+                                    <span>New</span>
+                                </div>
                                 <a class="ps-shoe__favorite" href="#"><i class="ps-icon-heart"></i></a><img
                                     src="{{ asset('assets/frontend/images/shoe/a1.avif') }}" alt="" /><a
                                     class="ps-shoe__overlay" href="product-detail.html"></a>
@@ -590,11 +593,9 @@
                             <div class="ps-shoe__content">
                                 <div class="ps-shoe__variants">
                                     <div class="ps-shoe__variant normal">
-                                        <img 
-                                        src="{{ asset('assets/frontend/images/shoe/2.jpg') }}" alt="" /><img
+                                        <img src="{{ asset('assets/frontend/images/shoe/2.jpg') }}" alt="" /><img
                                             src="images/shoe/3.jpg" alt="" /><img src=""
-                                            alt="" />
-                                            <img src="{{ asset('assets/frontend/images/shoe/4.jpg') }}"
+                                            alt="" /><img src="{{ asset('assets/frontend/images/shoe/4.jpg') }}"
                                             alt="" />
                                     </div>
                                     <select class="ps-rating ps-shoe__rating">
@@ -620,7 +621,9 @@
                      <div class="ps-shoes--carousel">
                         <div class="ps-shoe">
                             <div class="ps-shoe__thumbnail">
-                              
+                                <div class="ps-badge">
+                                    <span>New</span>
+                                </div>
                                 <a class="ps-shoe__favorite" href="#"><i class="ps-icon-heart"></i></a><img
                                     src="{{ asset('assets/frontend/images/shoe/a1.avif') }}" alt="" /><a
                                     class="ps-shoe__overlay" href="product-detail.html"></a>
@@ -628,11 +631,9 @@
                             <div class="ps-shoe__content">
                                 <div class="ps-shoe__variants">
                                     <div class="ps-shoe__variant normal">
-                                        <img 
-                                        src="{{ asset('assets/frontend/images/shoe/2.jpg') }}" alt="" /><img
+                                        <img src="{{ asset('assets/frontend/images/shoe/2.jpg') }}" alt="" /><img
                                             src="images/shoe/3.jpg" alt="" /><img src=""
-                                            alt="" />
-                                            <img src="{{ asset('assets/frontend/images/shoe/4.jpg') }}"
+                                            alt="" /><img src="{{ asset('assets/frontend/images/shoe/4.jpg') }}"
                                             alt="" />
                                     </div>
                                     <select class="ps-rating ps-shoe__rating">
@@ -655,11 +656,51 @@
                             </div>
                         </div>
                     </div>
+                     <div class="ps-shoes--carousel">
+                        <div class="ps-shoe">
+                            <div class="ps-shoe__thumbnail">
+                                <div class="ps-badge">
+                                    <span>New</span>
+                                </div>
+                                <a class="ps-shoe__favorite" href="#"><i class="ps-icon-heart"></i></a><img
+                                    src="{{ asset('assets/frontend/images/shoe/a1.avif') }}" alt="" /><a
+                                    class="ps-shoe__overlay" href="product-detail.html"></a>
+                            </div>
+                            <div class="ps-shoe__content">
+                                <div class="ps-shoe__variants">
+                                    <div class="ps-shoe__variant normal">
+                                        <img src="{{ asset('assets/frontend/images/shoe/2.jpg') }}" alt="" /><img
+                                            src="images/shoe/3.jpg" alt="" /><img src=""
+                                            alt="" /><img src="{{ asset('assets/frontend/images/shoe/4.jpg') }}"
+                                            alt="" />
+                                    </div>
+                                    <select class="ps-rating ps-shoe__rating">
+                                        <option value="1">1</option>
+                                        <option value="1">2</option>
+                                        <option value="1">3</option>
+                                        <option value="1">4</option>
+                                        <option value="2">5</option>
+                                    </select>
+                                </div>
+                                <div class="ps-shoe__detail">
+                                    <a class="ps-shoe__name" href="product-detai.html">Air Jordan 7 Retro</a>
+                                    <p class="ps-shoe__categories">
+                                        <a href="#">Men shoes</a>,<a href="#">
+                                            Nike</a>,<a href="#"> Jordan</a>
+                                    </p>
+                                    <span class="ps-shoe__price">
+                                        £ 120</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                   
                 </div>
             </div>
         </div>
     </div>
-
+ 
+   
     {{-- //size modal   --}}
     <div class="modal fade halo_modal-custom" id="size_chart" role="dialog">
         <div class="modal-dialog modal-dialog-centered">
