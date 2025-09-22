50557174e1008c6b00bcc936cc555b23ae96bca4
alamin <alamincsetpi@gmail.com>
2025-09-22 23:59:15 +0600

    feat: add the blog section

--- resources/views/front/blog/index.blade.php
+++ resources/views/front/blog/index.blade.php
@@ -4,9 +4,11 @@
         <div class="ps-container">
           <div class="row">
                 <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12 ">
-                  @foreach ($posts ?? [] as $post )
-                    
-                  <div class="ps-post--2">
+                  {{-- @foreach ($posts ?? [] as $post ) --}}
+                    <div id="post-list">
+                  @include('front.blog.list', ['posts' => $posts])
+                  </div>
+                  {{-- <div class="ps-post--2">
                     <div class="ps-post__thumbnail"><a class="ps-post__overlay" href="#"></a><img src="{{ asset('assets/frontend/images/blog/4.jpg')}}" alt=""></div>
                     <div class="ps-post__container">
                       <header class="ps-post__header"><a class="ps-post__title" href="">{{ $post->title }}</a>
@@ -22,43 +24,30 @@
                        
                       </footer>
                     </div>
-                  </div>
+                  </div> --}}
                 
-                  @endforeach
+                  {{-- @endforeach --}}
 
                   <div class="mt-30">
-                    <div class="ps-pagination">
-                      <ul class="pagination">
-                        <li><a href="#"><i class="fa fa-angle-left"></i></a></li>
-                        <li class="active"><a href="#">1</a></li>
-                        <li><a href="#">2</a></li>
-                        <li><a href="#">3</a></li>
-                        <li><a href="#">...</a></li>
-                        <li><a href="#"><i class="fa fa-angle-right"></i></a></li>
-                      </ul>
+                    <div class="#">
+                      {{-- {{  $posts->links() }} --}}
                     </div>
                   </div>
                 </div>
                 <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 ">
-                  <aside class="ps-widget--sidebar ps-widget--search">
-                    <form class="ps-search--widget" action="do_action" method="post">
-                      <input class="form-control" type="text" placeholder="Search posts...">
-                      <button><i class="ps-icon-search"></i></button>
-                    </form>
-                  </aside>
+                
                   <aside class="ps-widget--sidebar">
                     <div class="ps-widget__header">
-                      <h3>Archive</h3>
+                      <h3>All Categories</h3>
                     </div>
                     <div class="ps-widget__content">
                       <ul class="ps-list--arrow">
-                        <li class="current"><a href="product-listing.html">Shoe(321)</a></li>
-                        <li><a href="product-listing.html">Amazin’ Glazin’</a></li>
-                        <li><a href="product-listing.html">The Crusty Croissant</a></li>
-                        <li><a href="product-listing.html">The Rolling Pin</a></li>
-                        <li><a href="product-listing.html">Skippity Scones</a></li>
-                        <li><a href="product-listing.html">Mad Batter</a></li>
-                        <li><a href="product-listing.html">Confection Connection</a></li>
+                        @foreach ($categories ?? [] as  $category)
+                          <li class="current">
+                            <a href="{{ route('front.blog', ['category' => $category->slug]) }}">
+                           {{ $category->name }} ({{ $category->posts_count }}) 
+                        </a></li>
+                        @endforeach
                       </ul>
                     </div>
                   </aside>
@@ -70,7 +59,7 @@
                     <div class="ps-widget__content">
                       @foreach ($recent_posts ?? [] as  $recent_post)
                       <div class="ps-post--sidebar">
-                        <div class="ps-post__content"><a class="ps-post__title" href="{{ route('front.blog.details',$recent_post->slug) }}">{{ $recent_post->title }}</a><span>{{ $post->created_at->format('M d, Y') }}</span></div>
+                        <div class="ps-post__content"><a class="ps-post__title" href="{{ route('front.blog.details',$recent_post->slug) }}">{{ $recent_post->title }}</a><span>{{ $recent_post->created_at->format('M d, Y') }}</span></div>
                       </div>
                       @endforeach
                     </div>
@@ -93,3 +82,22 @@
         </div>
       </div>
 @endsection
+
+@push('js')
+<script>
+$(document).on('click', '.ps-list--arrow a, .ps-tags a, .pagination a', function(e) {
+    e.preventDefault();
+    let url = $(this).attr('href');
+
+    $.ajax({
+        url: url,
+        type: "GET",
+        success: function(data) {
+            $('#post-list').html(data);
+        }
+    });
+});
+
+</script>
+  
+@endpush
