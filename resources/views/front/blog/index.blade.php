b3602e58bf7a4b70b928b9cac4059609a3ed019e
alamin <alamincsetpi@gmail.com>
2025-09-22 14:44:57 +0600

    feat: add the blogs section

--- resources/views/front/blog/index.blade.php
+++ resources/views/front/blog/index.blade.php
@@ -62,56 +62,20 @@
                       </ul>
                     </div>
                   </aside>
-                  <aside class="ps-widget--sidebar">
-                    <div class="ps-widget__header">
-                      <h3>Ads Banner</h3>
-                    </div>
-                    <div class="ps-widget__content"><a href="product-listing"><img src="images/offer/sidebar.jpg" alt=""></a></div>
-                  </aside>
+                
                   <aside class="ps-widget--sidebar">
                     <div class="ps-widget__header">
                       <h3>Recent Posts</h3>
                     </div>
                     <div class="ps-widget__content">
+                      @foreach ($recent_posts ?? [] as  $recent_post)
                       <div class="ps-post--sidebar">
-                        <div class="ps-post__thumbnail"><a href="#"></a><img src="images/blog/sidebar/1.jpg" alt=""></div>
-                        <div class="ps-post__content"><a class="ps-post__title" href="#">Micenas Placerat Nibh Loreming Fentum</a><span>SEP 29, 2017</span></div>
+                        <div class="ps-post__content"><a class="ps-post__title" href="{{ route('front.blog.details',$recent_post->slug) }}">{{ $recent_post->title }}</a><span>{{ $post->created_at->format('M d, Y') }}</span></div>
                       </div>
-                      <div class="ps-post--sidebar">
-                        <div class="ps-post__thumbnail"><a href="#"></a><img src="images/blog/sidebar/2.jpg" alt=""></div>
-                        <div class="ps-post__content"><a class="ps-post__title" href="#">Micenas Placerat Nibh Loreming Fentum</a><span>SEP 29, 2017</span></div>
-                      </div>
-                      <div class="ps-post--sidebar">
-                        <div class="ps-post__thumbnail"><a href="#"></a><img src="images/blog/sidebar/3.jpg" alt=""></div>
-                        <div class="ps-post__content"><a class="ps-post__title" href="#">Micenas Placerat Nibh Loreming Fentum</a><span>SEP 29, 2017</span></div>
-                      </div>
+                      @endforeach
                     </div>
                   </aside>
-                  <aside class="ps-widget--sidebar">
-                    <div class="ps-widget__header">
-                      <h3>Best Seller</h3>
-                    </div>
-                    <div class="ps-widget__content">
-                      <div class="ps-shoe--sidebar">
-                        <div class="ps-shoe__thumbnail"><a href="#"></a><img src="images/shoe/sidebar/1.jpg" alt=""></div>
-                        <div class="ps-shoe__content"><a class="ps-shoe__title" href="#">Men's Shoe</a>
-                          <p><del> £253.00</del> £152.00</p><a class="ps-btn" href="#">PURCHASE</a>
-                        </div>
-                      </div>
-                      <div class="ps-shoe--sidebar">
-                        <div class="ps-shoe__thumbnail"><a href="#"></a><img src="images/shoe/sidebar/2.jpg" alt=""></div>
-                        <div class="ps-shoe__content"><a class="ps-shoe__title" href="#">Nike Flight Bonafide</a>
-                          <p><del> £253.00</del> £152.00</p><a class="ps-btn" href="#">PURCHASE</a>
-                        </div>
-                      </div>
-                      <div class="ps-shoe--sidebar">
-                        <div class="ps-shoe__thumbnail"><a href="#"></a><img src="images/shoe/sidebar/3.jpg" alt=""></div>
-                        <div class="ps-shoe__content"><a class="ps-shoe__title" href="#">Nike Sock Dart QS</a>
-                          <p><del> £253.00</del> £152.00</p><a class="ps-btn" href="#">PURCHASE</a>
-                        </div>
-                      </div>
-                    </div>
-                  </aside>
+                
                   <aside class="ps-widget--sidebar">
                     <div class="ps-widget__header">
                       <h3>Tags</h3>
@@ -128,120 +92,4 @@
           </div>
         </div>
       </div>
-      {{-- <div class="ps-subscribe">
-        <div class="ps-container">
-          <div class="row">
-                <div class="col-lg-3 col-md-12 col-sm-12 col-xs-12 ">
-                  <h3><i class="fa fa-envelope"></i>Sign up to Newsletter</h3>
-                </div>
-                <div class="col-lg-5 col-md-7 col-sm-12 col-xs-12 ">
-                  <form class="ps-subscribe__form" action="do_action" method="post">
-                    <input class="form-control" type="text" placeholder="">
-                    <button>Sign up now</button>
-                  </form>
-                </div>
-                <div class="col-lg-4 col-md-5 col-sm-12 col-xs-12 ">
-                  <p>...and receive  <span>$20</span>  coupon for first shopping.</p>
-                </div>
-          </div>
-        </div>
-      </div> --}}
-      <div class="ps-footer bg--cover" data-background="images/background/parallax.jpg">
-        <div class="ps-footer__content">
-          <div class="ps-container">
-            <div class="row">
-                  <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 ">
-                    <aside class="ps-widget--footer ps-widget--info">
-                      <header><a class="ps-logo" href="index.html"><img src="images/logo-white.png" alt=""></a>
-                        <h3 class="ps-widget__title">Address Office 1</h3>
-                      </header>
-                      <footer>
-                        <p><strong>460 West 34th Street, 15th floor, New York</strong></p>
-                        <p>Email: <a href='mailto:support@store.com'>support@store.com</a></p>
-                        <p>Phone: +323 32434 5334</p>
-                        <p>Fax: ++323 32434 5333</p>
-                      </footer>
-                    </aside>
-                  </div>
-                  <div class="col-lg-3 col-md-3 col-sm-12 col-xs-12 ">
-                    <aside class="ps-widget--footer ps-widget--info second">
-                      <header>
-                        <h3 class="ps-widget__title">Address Office 2</h3>
-                      </header>
-                      <footer>
-                        <p><strong>PO Box 16122 Collins  Victoria 3000 Australia</strong></p>
-                        <p>Email: <a href='mailto:support@store.com'>support@store.com</a></p>
-                        <p>Phone: +323 32434 5334</p>
-                        <p>Fax: ++323 32434 5333</p>
-                      </footer>
-                    </aside>
-                  </div>
-                  <div class="col-lg-2 col-md-2 col-sm-4 col-xs-12 ">
-                    <aside class="ps-widget--footer ps-widget--link">
-                      <header>
-                        <h3 class="ps-widget__title">Find Our store</h3>
-                      </header>
-                      <footer>
-                        <ul class="ps-list--link">
-                          <li><a href="#">Coupon Code</a></li>
-                          <li><a href="#">SignUp For Email</a></li>
-                          <li><a href="#">Site Feedback</a></li>
-                          <li><a href="#">Careers</a></li>
-                        </ul>
-                      </footer>
-                    </aside>
-                  </div>
-                  <div class="col-lg-2 col-md-2 col-sm-4 col-xs-12 ">
-                    <aside class="ps-widget--footer ps-widget--link">
-                      <header>
-                        <h3 class="ps-widget__title">Get Help</h3>
-                      </header>
-                      <footer>
-                        <ul class="ps-list--line">
-                          <li><a href="#">Order Status</a></li>
-                          <li><a href="#">Shipping and Delivery</a></li>
-                          <li><a href="#">Returns</a></li>
-                          <li><a href="#">Payment Options</a></li>
-                          <li><a href="#">Contact Us</a></li>
-                        </ul>
-                      </footer>
-                    </aside>
-                  </div>
-                  <div class="col-lg-2 col-md-2 col-sm-4 col-xs-12 ">
-                    <aside class="ps-widget--footer ps-widget--link">
-                      <header>
-                        <h3 class="ps-widget__title">Products</h3>
-                      </header>
-                      <footer>
-                        <ul class="ps-list--line">
-                          <li><a href="#">Shoes</a></li>
-                          <li><a href="#">Clothing</a></li>
-                          <li><a href="#">Accessries</a></li>
-                          <li><a href="#">Football Boots</a></li>
-                        </ul>
-                      </footer>
-                    </aside>
-                  </div>
-            </div>
-          </div>
-        </div>
-        <div class="ps-footer__copyright">
-          <div class="ps-container">
-            <div class="row">
-                  <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 ">
-                    <p>&copy; <a href="#">NOUTHEMES</a>, Inc. All rights Resevered. Design by <a href="#"> Alena Studio</a></p>
-                  </div>
-                  <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 ">
-                    <ul class="ps-social">
-                      <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                      <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
-                      <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                      <li><a href="#"><i class="fa fa-instagram"></i></a></li>
-                    </ul>
-                  </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
 @endsection
