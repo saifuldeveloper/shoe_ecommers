c4a5a9368d3202447b83f7d043c94abad95336fe
alamin <alamincsetpi@gmail.com>
2025-10-18 17:26:33 +0600

    payment method seeder

--- database/seeders/PaymentSettingsTableSeeder.php
+++ database/seeders/PaymentSettingsTableSeeder.php
@@ -18,6 +18,7 @@ class PaymentSettingsTableSeeder extends Seeder
 
         \DB::table('payment_settings')->delete();
         
+
         \DB::table('payment_settings')->insert(array (
             0 => 
             array (
@@ -27,6 +28,19 @@ class PaymentSettingsTableSeeder extends Seeder
                 'unique_keyword' => 'cod',
                 'photo' => '1631032407index.png',
                 'text' => 'Cash on Delivery basically means you will pay the amount of product while you get the item delivered to you.',
+                'status' => 'active',
+                'created_at' => NULL,
+                'updated_at' => NULL,
+            ),
+            
+            1 => 
+            array (
+                'id' => 2, 
+                'name' => 'SSLCOMMERZ',
+                'information' => NULL, 
+                'unique_keyword' => 'sslcommerz', 
+                'photo' => 'sslcommerz_logo.png', 
+                'text' => 'After clicking "Pay now" - you will be redirected to SSLCOMMERZ to complete your purchase securely.',
                 'status' => 'active',
                 'created_at' => NULL,
                 'updated_at' => NULL,
