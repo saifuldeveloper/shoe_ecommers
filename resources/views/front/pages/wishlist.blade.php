6eaf18e96f2bd0403abdbeab458ca1e5eaa851b7
alamin <alamincsetpi@gmail.com>
2025-12-21 13:35:48 +0600

    feat: left side image shows

--- resources/views/front/pages/wishlist.blade.php
+++ resources/views/front/pages/wishlist.blade.php
@@ -3,10 +3,11 @@
  <div class="ps-content pt-80 pb-80">
         <div class="ps-container">
           <div class="ps-cart-listing ps-table--whishlist">
-            <table class="table ps-cart__table">
+            <table class="table ps-cart__table" class="table ">
                       @include('alerts.alerts')
               <thead>
                 <tr>
+                  <th>Image</th>
                   <th>All Products</th>
                   <th>Price</th>
                   <th>Availability</th>
@@ -18,6 +19,16 @@
             @if ($wishlist_items->count() > 0)
                 @foreach ($wishlist_items as $product)
                 <tr>
+                   <td>
+                    <a href="{{route('front.product',$product->slug)}}">
+                  <img src="{{ $product->thumbnail
+                      ? (file_exists(public_path('storage/items/' . $product->thumbnail))
+                          ? url('storage/items/' . $product->thumbnail)
+                          : url('assets/images/' . $product->thumbnail))
+                      : url('assets/images/default.jpg') }}"
+                      alt="Image Not Found">
+                      </a>
+              </td>
                   <td><a class="ps-product__preview" href="{{route('front.product',$product->slug)}}"> {{$product->name}}</a></td>
                   <td>{{PriceHelper::grandCurrencyPrice($product)}} </td>
                    <td>
