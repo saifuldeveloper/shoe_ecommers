4518ae160ebf0be97370132358be6f4e8b7b4e05
saifuldeveloper <mdsaiful77336@gmail.com>
2026-01-08 09:13:47 +0600

    product details page currection

--- resources/views/front/pages/product_detail.blade.php
+++ resources/views/front/pages/product_detail.blade.php
@@ -205,153 +205,114 @@
                             <div class="ps-product__content mt-100">
                                 <ul class="tab-list" role="tablist">
                                     <li class="active">
-                                        <a href="#tab_01" aria-controls="tab_01" role="tab"
-                                            data-toggle="tab">Overview</a>
-                                    </li>
-                                    {{-- <li>
-                                        <a href="#tab_02" aria-controls="tab_02" role="tab"
-                                            data-toggle="tab">Review</a>
-                                    </li> --}}
-                                    <li>
-                                        <a href="#tab_03" aria-controls="tab_03" role="tab"
-                                            data-toggle="tab">PRODUCT TAG</a>
+                                        <a href="#tab_01" aria-controls="tab_01" role="tab" data-toggle="tab"></a>
+                                        Description</a>
                                     </li>
                                     <li>
-                                        <a href="#tab_04" aria-controls="tab_04" role="tab"
-                                            data-toggle="tab">ADDITIONAL</a>
+                                        <a href="#tab_03" aria-controls="tab_03" role="tab" data-toggle="tab">Terms
+                                            and Conditions</a>
+                                    </li>
+                                    <li>
+                                        <a href="#tab_04" aria-controls="tab_04" role="tab" data-toggle="tab">Size
+                                            Chart</a>
                                     </li>
                                 </ul>
                             </div>
                             <div class="tab-content mb-60">
                                 <div class="tab-pane active" role="tabpanel" id="tab_01">
-                                    <p>
-                                        Caramels tootsie roll carrot cake sugar
-                                        plum. Sweet roll jelly bear claw
-                                        liquorice. Gingerbread lollipop dragée
-                                        cake. Pie topping jelly-o. Fruitcake
-                                        dragée candy canes tootsie roll. Pastry
-                                        jelly-o cupcake. Bonbon brownie soufflé
-                                        muffin.
+                                {!! $item_details->details !!}
                                     </p>
-                                    <p>
-                                        Sweet roll soufflé oat cake apple pie
-                                        croissant. Pie gummi bears jujubes cake
-                                        lemon drops gummi bears croissant
-                                        macaroon pie. Fruitcake tootsie roll
-                                        chocolate cake Carrot cake cake bear
-                                        claw jujubes topping cake apple pie.
-                                        Jujubes gummi bears soufflé candy canes
-                                        topping gummi bears cake soufflé cake.
-                                        Cotton candy soufflé sugar plum pastry
-                                        sweet roll..
-                                    </p>
-                                </div>
-                                <div class="tab-pane" role="tabpanel" id="tab_02">
-                                    <p class="mb-20">
-                                        1 review for
-                                        <strong>Shoes Air Jordan</strong>
-                                    </p>
-                                    <div class="ps-review">
-                                        <div class="ps-review__thumbnail">
-                                            <img src="images/user/1.jpg" alt="" />
-                                        </div>
-                                        <div class="ps-review__content">
-                                            <header>
-                                                <select class="ps-rating">
-                                                    <option value="1">1</option>
-                                                    <option value="1">2</option>
-                                                    <option value="1">3</option>
-                                                    <option value="1">4</option>
-                                                    <option value="5">5</option>
-                                                </select>
-                                                <p>
-                                                    By<a href="">
-                                                        Alena Studio</a>
-                                                    - November 25, 2017
-                                                </p>
-                                            </header>
-                                            <p>
-                                                Soufflé danish gummi bears tart.
-                                                Pie wafer icing. Gummies jelly
-                                                beans powder. Chocolate bar
-                                                pudding macaroon candy canes
-                                                chocolate apple pie chocolate
-                                                cake. Sweet caramels sesame
-                                                snaps halvah bear claw wafer.
-                                                Sweet roll soufflé muffin
-                                                topping muffin brownie. Tart
-                                                bear claw cake tiramisu
-                                                chocolate bar gummies dragée
-                                                lemon drops brownie.
-                                            </p>
-                                        </div>
-                                    </div>
-                                    <form class="ps-product__review" action="_action" method="post">
-                                        <h4>ADD YOUR REVIEW</h4>
-                                        <div class="row">
-                                            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-                                                <div class="form-group">
-                                                    <label>Name:<span>*</span></label>
-                                                    <input class="form-control" type="text" placeholder="" />
-                                                </div>
-                                                <div class="form-group">
-                                                    <label>Email:<span>*</span></label>
-                                                    <input class="form-control" type="email" placeholder="" />
-                                                </div>
-                                                <div class="form-group">
-                                                    <label>Your rating<span></span></label>
-                                                    <select class="ps-rating">
-                                                        <option value="1">
-                                                            1
-                                                        </option>
-                                                        <option value="1">
-                                                            2
-                                                        </option>
-                                                        <option value="1">
-                                                            3
-                                                        </option>
-                                                        <option value="1">
-                                                            4
-                                                        </option>
-                                                        <option value="5">
-                                                            5
-                                                        </option>
-                                                    </select>
-                                                </div>
-                                            </div>
-                                            <div class="col-lg-8 col-md-8 col-sm-6 col-xs-12">
-                                                <div class="form-group">
-                                                    <label>Your Review:</label>
-                                                    <textarea class="form-control" rows="6"></textarea>
-                                                </div>
-                                                <div class="form-group">
-                                                    <button class="ps-btn ps-btn--sm">
-                                                        Submit<i class="ps-icon-next"></i>
-                                                    </button>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </form>
                                 </div>
                                 <div class="tab-pane" role="tabpanel" id="tab_03">
-                                    <p>Add your tag <span> *</span></p>
-                                    <form class="ps-product__tags" action="_action" method="post">
-                                        <div class="form-group">
-                                            <input class="form-control" type="text" placeholder="" />
-                                            <button class="ps-btn ps-btn--sm">
-                                                Add Tags
-                                            </button>
-                                        </div>
-                                    </form>
+                                    <p>
+                                        Dear Customer, we try our best from our end to provide you the best customer
+                                        experience.
+                                        <br>
+
+                                        **The delivery of your order is subjected to availability of stock.
+                                        <br>
+
+                                        **Check and receive your product in front of the delivery man. If your product is
+                                        damaged, defective, incorrect or has any mismatch in size or pricing issue at the
+                                        time of delivery, please return it immediately to the delivery person.
+                                        <br>
+
+
+                                        For any assistance, you can call our customer service at 09666200300. (9.00am to
+                                        9.00pm except Govt. holidays) But in case if you couldn't reach the customer
+                                        service, you can provide your issue details after selecting the proper issue section
+                                        in our Report Issue Section.
+
+
+                                        <br>
+                                        Ordering Policy
+                                        Service is only available for articles (products) which are shown in stock on our
+                                        website
+                                        The delivery of your order is subjected to availability of stock.
+                                        Only one product should be booked in one order, in case of any customer needs to
+                                        order multiple products or same products in multiple quantity, customers will need
+                                        to place multiple separate orders.
+                                        Currently, each order will be shipped only to a single destination address. If
+                                        customers wish to ship products to different addresses, customers will need to place
+                                        multiple separate orders.
+                                        Orders that are paid for cannot be cancelled.
+                                        For Order related queries, please kindly contact our Customer Service at
+                                        09666200300. (9.00am to 9.00pm except Govt. holidays).
+                                        <hr>
+                                        <br>
+                                        Shipping Policy
+                                        We are committed to delivering your order accurately, in good condition, and on
+                                        time. While we will endeavor to ship all items in your order together, this may not
+                                        always be possible due to product characteristics, or stock availability.
+
+
+
+                                        Please note our shipping policy as follows:
+
+                                        We make our best efforts to ship each item in your order within 3 to 5 working days
+                                        inside Dhaka metropolitan city and 7 to 10 working days outside Dhaka.
+                                        We ship on regular week days (Sunday to Thursday), excluding public holidays.
+                                        Currently, each order will be shipped only to a single destination address. If
+                                        customers wish to ship products to different addresses, customers will need to place
+                                        multiple separate orders.
+                                        While we endeavor to ship all items your order accurately, in good condition, and on
+                                        time, your order may get canceled or delivery may get delayed due to size
+                                        unavailability, product unavailability or courier issues.
+                                        To ensure that your order reaches you in the fastest time and in good condition, we
+                                        only ship through reputed courier agencies
+
+
+                                        Size Change
+                                        Dear Customer,
+
+                                        We are committed to ensure your satisfaction with any merchandise you have ordered
+                                        from Avijatry.com. The delivery of your order is subjected to availability of stock.
+                                        You are expected to check and receive your product in front of the delivery man. If
+                                        your product is damaged, defective, incorrect or has any mismatch in size or pricing
+                                        issue at the time of delivery, please return it immediately to the delivery person.
+
+                                        If the size doesn't fit, you can exchange the size of the product bought from Bata
+                                        E-Commerce site within 7 days from the delivery date. We will not accept the
+                                        exchange request if you request for exchange after 7 days from the delivery date.
+
+                                        We will only accept exchange that the product(s) being returned for exchange is
+                                        unused, clean, and in their original state and packaging. In this case please
+                                        contact our customer service regarding which store can be the most convenient one
+                                        for you to get the replacement from depending on the product, size and stock
+                                        availability.
+
+                                        You have to call our customer service at 09666200300. (9.00am to 9.00pm except Govt.
+                                        holidays) to initiate the exchange.
+
+                                        An exchange cannot be done with a product of a different model.
+
+                                        Please keep your invoice safe and intact as it is essential to give the same to the
+                                        store while getting your product exchanged.
+                                    </p>
                                 </div>
                                 <div class="tab-pane" role="tabpanel" id="tab_04">
-                                    <div class="form-group">
-                                        <textarea class="form-control" rows="6" placeholder="Enter your addition here..."></textarea>
-                                    </div>
-                                    <div class="form-group">
-                                        <button class="ps-btn" type="button">
-                                            Submit
-                                        </button>
+                                    <div>
+                                        <img src="{{ asset('assets/images/sizechart.webp') }}" alt="">
                                     </div>
                                 </div>
                             </div>
@@ -452,7 +413,7 @@
                         <div class="modal-body">
                             <div class="size-chart-img">
 
-                                <img src="//www.batabd.com/cdn/shop/files/size-chart-new_ca42ba2e-41a3-4df9-9c1a-ddfa2e4114e6_1024x1024.jpg?v=1745834022"
+                                <img src="//www.Avijatry.com/cdn/shop/files/size-chart-new_ca42ba2e-41a3-4df9-9c1a-ddfa2e4114e6_1024x1024.jpg?v=1745834022"
                                     alt="Size Chart" />
 
                             </div>
